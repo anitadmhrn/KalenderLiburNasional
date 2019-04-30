fedaa686697fb29290b40350ec9bb3742295d4e6
Anita Dwi Maharani <37867025+anitadmhrn@users.noreply.github.com>
2019-04-30 17:54:39 +0700

    Add files via upload

---
+++ Kalender/app/src/main/java/com/lenovo/kalender/MainActivity.java
@@ -0,0 +1,39 @@
+package com.lenovo.kalender;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setMinimumDate(CalendarDay.from(1990,1,1))
+                .setMaximumDate(CalendarDay.from(2150,12,31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(MainActivity.this, ""+date, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
